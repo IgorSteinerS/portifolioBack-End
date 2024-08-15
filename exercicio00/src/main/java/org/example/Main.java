a6e69f1fbde5363a71317b1b46c0741a0f040fe2
Igor Steiner de Souza <igorsteinerdesouza@gmail.com>
2024-08-15 19:28:20 -0300

    exercicio00 feito

--- exercicio00/src/main/java/org/example/Main.java
+++ exercicio00/src/main/java/org/example/Main.java
@@ -1,7 +1,26 @@
 package org.example;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        while (true) {
+            Scanner leitor = new Scanner(System.in);
+
+            System.out.println("Digite seu primeiro nome:");
+            String primeiroNome = leitor.nextLine();
+            if(primeiroNome.isEmpty()) return;
+            System.out.println("Digite seu segundo nome:");
+            String segundoNome = leitor.nextLine();
+
+            if(segundoNome.isEmpty()) return;
+
+            Random geradorAleatorio = new Random();
+            int numeroAleatorio = geradorAleatorio.nextInt(10,100);
+
+            System.out.println(primeiroNome + segundoNome + numeroAleatorio);
+
+        }
     }
 }
