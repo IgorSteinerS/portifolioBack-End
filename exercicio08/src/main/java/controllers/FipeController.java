f7815f065f793c71280f14085161d591dba898df
Igor Steiner de Souza <igorsteinerdesouza@gmail.com>
2024-09-18 20:10:09 -0300

    exercicio08 "feito"
    precisa de correção

---
+++ exercicio08/src/main/java/controllers/FipeController.java
@@ -0,0 +1,38 @@
+package controllers;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestClient;
+
+import java.util.HashMap;
+
+@RestController
+@RequestMapping("/fipe")
+public class FipeController {
+
+    private RestClient cliente = RestClient.create("https://parallelum.com.br");
+
+    @GetMapping("/valor")
+    @ResponseBody //Transforma em json
+    public HashMap<String, String> valor(@RequestParam String marca,
+                                         @RequestParam String modelo,
+                                         @RequestParam String ano){
+
+
+        HashMap<String, String> resposta = cliente
+                .get()
+                .uri("/fipe/api/v1/carros/marcas/"+ marca +"/modelos/"+ modelo +"/anos/" + ano)
+                .retrieve()
+                .body(HashMap.class);
+
+
+
+        HashMap<String, String> respostaPersonalizada = new HashMap<>();
+        respostaPersonalizada.put("Valor", resposta.get("Valor"));
+        respostaPersonalizada.put("MesReferencia",resposta.get("MesReferencia"));
+
+//fipe/valor?marca=59&modelo=5940&ano=2014-3
+
+
+        return respostaPersonalizada;
+    }
+}
